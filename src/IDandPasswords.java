7cd31acadf6a1f6314baa787a8933d2d91a53316
Jabed <jabedhossain1200@gmail.com>
2024-02-11 10:46:33 +0600

    First commit

--- src/IDandPasswords.java
+++ src/IDandPasswords.java
@@ -1,2 +1,24 @@
+import java.util.HashMap;
+
 public class IDandPasswords {
+
+    HashMap<String,String> logininfo = new HashMap<String,String>();
+
+    IDandPasswords(){
+        logininfo.put("Jabed", "Burger");
+        logininfo.put("Noor", "Legpiece");
+        logininfo.put("Shohag", "Spice");
+    }
+
+    protected HashMap getLoginInfo(){
+        return logininfo;
+    }
+
+
+
+
+
+
+
+
 }
--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class WelcomePage {
+public class WelcomePage {
 }
